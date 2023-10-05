c8d3558bb5576c056d37cd4f201695ac1fb55ef7
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-10-05 12:59:45 +0300

    feat: add solution for homework 6

--- src/HOC/withJSHighlight.tsx
+++ src/HOC/withJSHighlight.tsx
@@ -2,11 +2,13 @@ import { FC, useEffect } from 'react';
 import hljs from 'highlight.js/lib/core';
 import 'highlight.js/styles/default.css';
 import javascript from 'highlight.js/lib/languages/javascript';
+import html from 'highlight.js/lib/languages/xml';
 
 export const withJSHighlight = <P extends object>(Component: FC<P>) => {
   const ComponentWithHighlight: FC<P> = (props) => {
     useEffect(() => {
       hljs.registerLanguage('javascript', javascript);
+      hljs.registerLanguage('html', html);
       hljs.highlightAll();
     }, []);
 
--- src/components/tasks/index.ts
+++ src/components/tasks/index.ts
@@ -3,3 +3,4 @@ export * from './task-02';
 export * from './task-03';
 export * from './task-04';
 export * from './task-05';
+export * from './task-06';
